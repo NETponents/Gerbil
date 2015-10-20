a56cbfaa3ceec0838b056ba0a57dfa6c7de673b7
Joshua Zenn <wordman05@gmail.com>
2015-10-20 10:10:11 -0400

    Update config.cs

--- src/config.cs
+++ src/config.cs
@@ -10,24 +10,46 @@ namespace Gerbil
     {
       configStore = new Dictionary<string,string>();
     }
-    public int loadConfig(string filepath)
+    public void loadConfig(string filepath)
     {
       string[] lines = System.IO.File.ReadAllLines(filepath);
       foreach(string line in lines)
       {
         string[] args = line.split("=");
-        this.storeField(args[0], args[1]);
+        configStore.Add(args[0], args[1]);
       }
     }
-    private void storeField(string id, string value)
+    public void reloadConfig(string filepath)
     {
-      configStore.Add(id, value);
+      string[] lines = System.IO.File.ReadAllLines(filepath);
+      foreach(string line in lines)
+      {
+        string[] args = line.split("=");
+        if(configStore.ContainsKey(args[0]))
+        {
+          configStore[args[0]] = args[1];
+        }
+        else
+        {
+          configStore.Add(args[0], args[1]);
+        }
+      }
+    }
+    public void commitConfig(string filepath)
+    {
+      List<string> tempFile = new List<string>();
+      foreach(KeyValuePair<string,string> sets in configStore)
+      {
+        tempFile.add(sets.Key + "=" + sets.Value);
+      }
+      //TODO: commit tempFile to actual settings file
+      this.reloadConfig(filepath);
     }
     public string getField(string id)
     {
       if(configStore.ContainsKey(id))
       {
-        return configStore.Get(id);
+        return configStore[id];
       }
       return "default";
     }
