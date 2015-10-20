be74c9ef8902aa0cd3e79792b1087864fa42b0c6
Joshua Zenn <wordman05@gmail.com>
2015-10-20 09:27:36 -0400

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,1 +1,10 @@
+#include <iostream>
 
+public static int main()
+{
+  std::cout << "Gerbil v0.1 Alpha\n";
+  std::cout << "Copyright 2015 GPL V3 License\n\n";
+  std::cout << "Beginning loading process";
+  
+  return 0;
+}
