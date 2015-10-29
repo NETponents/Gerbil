d733543ea5282d142312e6f242df357bc29e6198
Joshua Zenn <wordman05@gmail.com>
2015-10-29 09:41:18 -0400

    Create Gerbil_Attackers.cs

---
+++ Gerbil_Attackers.cs
@@ -0,0 +1,32 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Gerbil
+{
+    namespace Attackers
+    {
+        class Attacker
+        {
+            public Attacker()
+            {
+                
+            }
+            public virtual void init()
+            {
+                
+            }
+            public virtual void stab()
+            {
+                
+            }
+            public virtual void clean()
+            {
+                
+            }
+        }
+    }
+}
