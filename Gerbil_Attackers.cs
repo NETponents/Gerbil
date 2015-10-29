67e8f4ec436b73e23a8b5a67b1879e07a40c93c0
Joshua Zenn <wordman05@gmail.com>
2015-10-29 09:55:58 -0400

    Update Gerbil_Attackers.cs

--- Gerbil_Attackers.cs
+++ Gerbil_Attackers.cs
@@ -9,23 +9,77 @@ namespace Gerbil
 {
     namespace Attackers
     {
-        class Attacker
+        public enum AttackerResult
         {
+            Created,
+            Initialized,
+            FailedAuth,
+            FailedConnection,
+            Trying,
+            Connected,
+            Penetrated
+        };
+        public class AttackerNotInitializedException : Exception
+        {
+            
+        }
+        public class AttackerNoTargetFoundException : Exception
+        {
+            
+        }
+        public class AttackerAttemptsExhaustedException : Exception
+        {
+            
+        }
+        public class Attacker
+        {
+            private AttackerResult attackerStatus;
+            
+            /// <summary>
+            /// Constructor for Attacker class
+            /// </summary>
             public Attacker()
             {
-                
+                attackerStatus = AttackerResult.Created;
             }
+            /// <summary>
+            /// Performs initializing commands for attacker
+            /// </summary>
             public virtual void init()
             {
-                
+                attackerStatus = AttackerResult.Initialized;
             }
+            /// <summary>
+            /// Attacks the given client, will only attempt once
+            /// </summary>
             public virtual void stab()
             {
-                
+                if(attackerStatus == AttackerResult.Created)
+                {
+                    throw new AttackerNotInitializedException();
+                }
+                else if(attackerStatus ==  AttackerResult.FailedConnection)
+                {
+                    throw new AttackerTargetNotFoundException();
+                }
+                else if(attackerStatus == AttackerResult.FailedAuth)
+                {
+                    throw new AttackerAttemptsExhaustedException();
+                }
             }
+            /// <summary>
+            /// Deletes all evidence and closes connection to target
+            /// </summary>
             public virtual void clean()
             {
-                
+                if(attackerStatus == AttackerResult.Created)
+                {
+                    throw new AttackerNotInitializedException();
+                }
+                else if(attackerStatus ==  AttackerResult.FailedConnection)
+                {
+                    throw new AttackerTargetNotFoundException();
+                }
             }
         }
     }
